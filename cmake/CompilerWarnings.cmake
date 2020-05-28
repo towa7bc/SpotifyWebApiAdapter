7bbba91a9076e8e472bbe57ea37b3084da4f3ee6
MW <michi.wittmann@gmail.com>
2020-05-28 09:28:06 +0200

    MSVC no pragma warnings

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -38,6 +38,7 @@ function(set_project_warnings project_name)
           /w14928 # illegal copy-initialization; more than one user-defined
           # conversion has been implicitly applied
           /permissive- # standards conformance mode for MSVC compiler.
+          /wd4068
           )
 
   set(CLANG_WARNINGS
